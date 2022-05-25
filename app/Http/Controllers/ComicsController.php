eb17ac02029beb597eab35bd1c320c90d5d41dd9
emicif <96519927+emicif@users.noreply.github.com>
2022-05-25 15:47:07 +0200

    create seeder, view show, route

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Comic;
 
 class ComicsController extends Controller
 {
@@ -13,7 +14,7 @@ class ComicsController extends Controller
      */
     public function index()
     {
-        //
+        $data = Comic::all();
     }
 
     /**
--- database/seeds/ComicsTableSeeder.php
+++ database/seeds/ComicsTableSeeder.php
@@ -15,7 +15,15 @@ class ComicsTableSeeder extends Seeder
         $comics=config('comics');
         foreach($comics as $comic){
             $newComic = new Comic();
-            $newComic->
+            $newComic->title = $comic['title'];
+            $newComic->description = $comic['description'];
+            $newComic->thumb = $comic['thumb'];
+            $newComic->price = $comic['price'];
+            $newComic->series = $comic['series'];
+            $newComic->sale_date = $comic['sale_date'];
+            $newComic->type = $comic['type'];
+            $newComic->save();
+
         }
     }
 }
