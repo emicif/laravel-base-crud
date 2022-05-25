1e5760f2bb3bd5d9c3d9d6ce13a879864fd1942a
emicif <96519927+emicif@users.noreply.github.com>
2022-05-25 15:40:49 +0200

    migrate db

---
+++ database/seeds/ComicsTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Comic;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $comics=config('comics');
+        foreach($comics as $comic){
+            $newComic = new Comic();
+            $newComic->
+        }
+    }
+}
