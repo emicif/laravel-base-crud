f3a21725df5e92b9baa3e405062372baf9158561
emicif <96519927+emicif@users.noreply.github.com>
2022-05-25 17:25:01 +0200

    end

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -13,13 +13,13 @@
 
     <div class="p10">
         <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
-        <h3>{{ $comic->title }}</h3>
+        <h3 class="pt10">{{ $comic->title }}</h3>
         <div class="bg-green flex p10">
             <strong>
-                <p class="p10">{{ $comic->price }}</p>
+                <p class="p10">€{{ $comic->price }}</p>
             </strong>
             <strong>
-                <p class="p10">{{ $comic->series }}</p>
+                <p class="p10 ">{{ $comic->series }}</p>
             </strong>
             <strong>
                 <p class="p10">{{ $comic->sale_date }}</p>
@@ -27,7 +27,7 @@
 
         </div>
 
-        <p>{{ $comic->description }}</p>
+        <p class="pt10">{{ $comic->description }}</p>
         <button>
             <a href="{{ route('comics.index', $comic->id) }}">BACK</a>
         </button>
